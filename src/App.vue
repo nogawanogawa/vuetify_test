a35f0f7fda26e6d9090b233739cdb8cd9a087e4a
tsunotsuno <t_tsuno_tsuno@yahoo.co.jp>
2019-03-08 18:01:17 +0900

    sample

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" fixed app>
+      <v-list dense>
+        <v-list-tile @click="click_1(); click_2()">
+          <v-list-tile-action>
+            <v-icon>home</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Home</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="click_1(); click_2()">
+          <v-list-tile-action>
+            <v-icon>contact_mail</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Contact</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar color="indigo" dark fixed app>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-form>
+        <v-container>
+          <v-layout align-start justify-end row>
+            <v-flex xs12 sm6 md3>
+              <v-text-field label="Search"></v-text-field>
+            </v-flex>
+            <v-btn outline small fab color="indigo">
+              <v-icon>search</v-icon>
+            </v-btn>
+          </v-layout>
+        </v-container>
+      </v-form>
+
+      <Dialog/>
+    </v-content>
+    <v-footer color="indigo" app>
+      <span class="white--text">&copy; 2017</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Dialog from "./components/Dialog.vue";
 
 export default {
-  name: 'app',
+  name: "app",
+  data: () => ({
+    drawer: null
+  }),
   components: {
-    HelloWorld
+    Dialog
+  },
+  props: {
+    source: String
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
